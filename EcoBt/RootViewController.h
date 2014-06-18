e398c55ce9857c6e06142190642b42d5a3ce515a
derrickallenlo <dalo@uci.edu>
2014-06-17 21:13:07 -0700

    Initial Commit - Tutorial of CorePlot - http://www.possumpiggyproductions.com/CorePlotTutUpdate.htm

--- EcoBt/RootViewController.h
+++ EcoBt/RootViewController.h
@@ -7,9 +7,34 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "CorePlot-CocoaTouch.h"
+
+@protocol CPTPlotDataSource <NSObject>
+-(NSUInteger)numberOfRecordsForPlot:(CPTPlot *)plot;   // we implement this one
+
+@optional
+// Implement one of the following
+-(NSArray *)numbersForPlot:(CPTPlot *)plot
+                     field:(NSUInteger)fieldEnum
+          recordIndexRange:(NSRange)indexRange;
+
+-(NSNumber *)numberForPlot:(CPTPlot *)plot            // and this one
+                     field:(NSUInteger)fieldEnum
+               recordIndex:(NSUInteger)index;
+
+-(NSRange)recordIndexRangeForPlot:(CPTPlot *)plot
+                        plotRange:(CPTPlotRange *)plotRect;
+
+@end
 
 @interface RootViewController : UIViewController <UIPageViewControllerDelegate>
+{
+    CPTXYGraph *graph;
+    
+}
 
 @property (strong, nonatomic) UIPageViewController *pageViewController;
+
+
 
 @end
